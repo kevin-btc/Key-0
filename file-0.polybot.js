19fe83cef131d51fe258ce92e2457a5cc018dca4
kgricour <kevin@newton.so>
2023-04-04 15:03:46 +0200

    Test

---
+++ file-0.polybot.js
@@ -0,0 +1,5 @@
+fn add_numbers(x: i32, y: i32) -> i32 {
+  x + y
+}
+
+1xdfowfcqeq
---
+++ file-1.polybot.js
@@ -0,0 +1,9 @@
+fn factorial(n: u32) -> u32 {
+  match n {
+      0 => 1,
+      1 => 1,
+      _ => n * factorial(n - 1),
+  }
+}
+
+zalv7uuoczg
